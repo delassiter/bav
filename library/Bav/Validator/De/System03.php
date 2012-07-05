7f778573546203078f6d72d64aafd0ca3d122f77
Dennis Lassiter <dl@europaymentgroup.com>
2012-07-05 21:53:57 +0200

    Added System Validators

--- library/Bav/Validator/De/System03.php
+++ library/Bav/Validator/De/System03.php
@@ -4,22 +4,9 @@ namespace Bav\Validator\De;
 
 use Bav\Validator\Math;
 
-class System03 extends \Bav\Validator\IterationWeighted
+class System03 extends System01
 {
     
     protected $weights = array(2, 1);
-    protected $modulo = 10;
-    
-    
-    protected function iterationStep()
-    {
-        $this->accumulator += $this->number * $this->getWeight();
-    }
 
-    protected function getResult()
-    {
-        $result = $this->modulo - ($this->accumulator % $this->modulo);
-        $result = ($result == $this->modulo) ? 0 : $result;
-        return (string)$result === $this->getCheckNumber();
-    }
 }
